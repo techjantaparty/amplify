c868e130cc9f2a46a85e86d6406a2751944207d2
Subham Mani <manisubham09@gmail.com>
2025-02-23 00:18:36 +0530

    update

--- amplify/app/src/app/(protected)/home/page.tsx
+++ amplify/app/src/app/(protected)/home/page.tsx
@@ -4,14 +4,24 @@ import React from "react";
 import WithWalletProtection from "../withProtected";
 import AllPosts from "@/components/AllPosts";
 import CreatePost from "@/components/CreatePost";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="min-h-screen px-6 py-4 md:px-8 md:py-6 bg-gradient-to-bl from-[#151515] via-[#292929] to-[#151515]">
-      <h1 className="text-3xl font-bold">Welcome user!</h1>
-      <h2 className="text-lg font-medium">
-        See what people are reporting about
-      </h2>
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold">Welcome user!</h1>
+          <h2 className="text-lg font-medium">
+            See what people are reporting about
+          </h2>
+        </div>
+        <Link href={"/advocates"}>
+          <button className="shadow-2xl z-50 font-bold cursor-pointer border text-sm border-white hover:bg-white/5 p-4 rounded-md flex items-center justify-center gap-2">
+            Join as Advocate
+          </button>
+        </Link>
+      </div>
       <main className="grid grid-cols-1 lg:grid-cols-3 gap-6 my-12">
         {/* CreatePost appears first on mobile but stays on the right on large screens */}
         <div className="order-1 lg:order-2">
