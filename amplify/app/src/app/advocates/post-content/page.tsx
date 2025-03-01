12786bf6a85f1a095a4ad92a0815bc3cdfdce44e
Subham Mani <manisubham09@gmail.com>
2025-03-01 16:28:19 +0530

    update

--- amplify/app/src/app/advocates/post-content/page.tsx
+++ amplify/app/src/app/advocates/post-content/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const AdvocatesDashboard = () => {
   return (
-    <div>
+    <div className="px-20">
       <div className="flex justify-end bg-transparent">
         <Link
           className="px-4 py-2 border border-white rounded-md mt-6 mr-8"
@@ -14,7 +14,7 @@ const AdvocatesDashboard = () => {
           Profile
         </Link>
       </div>
-      <div className="relative min-h-screen flex flex-col-reverse lg:flex-row gap-4 justify-center p-6">
+      <div className="relative min-h-screen flex flex-col-reverse lg:flex-row gap-4 justify-center py-6">
         <LawyerPostCard />
         <div className="w-full max-w-2xl text-white p-8 rounded-xl">
           <PostContentForm />
